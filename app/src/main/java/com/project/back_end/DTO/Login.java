a0451b691b11e24ae37353bdd2c71f4903b48495
Rashid Amanzholov <rashid.amanzholov@gmail.com>
2025-07-15 11:56:12 +0200

    Create the Login DTO

--- app/src/main/java/com/project/back_end/DTO/Login.java
+++ app/src/main/java/com/project/back_end/DTO/Login.java
@@ -1,6 +1,41 @@
 package com.project.back_end.DTO;
 
 public class Login {
+    private String identifier;
+    private String password;
+
+    // Default constructor
+    public Login() {
+    }
+
+    // Parameterized constructor
+    public Login(String identifier, String password) {
+        this.identifier = identifier;
+        this.password = password;
+    }
+
+    // Getter and Setter for identifier
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    // Getter and Setter for password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
+// package com.project.back_end.DTO;
+
+// public class Login {
     
 // 1. 'email' field:
 //    - Type: private String
@@ -27,4 +62,4 @@ public class Login {
 //    - The 'setPassword(String password)' method sets the password value.
 
 
-}
+// }
