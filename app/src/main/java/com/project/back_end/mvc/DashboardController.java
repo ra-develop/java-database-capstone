53a5f7cba300ce99f71a7573d4bc89e55dc45668
Rashid Amanzholov <rashid.amanzholov@gmail.com>
2025-07-15 16:38:31 +0200

    Create main Service Class, Renamed "Service" to "AppService" class

--- app/src/main/java/com/project/back_end/mvc/DashboardController.java
+++ app/src/main/java/com/project/back_end/mvc/DashboardController.java
@@ -2,21 +2,25 @@ package com.project.back_end.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import com.project.back_end.services.AppService;
+
 import java.util.Map;
 
 @Controller
 public class DashboardController {
 
     @Autowired
-    private Service service;
+    private AppService service;
 
     @GetMapping("/adminDashboard/{token}")
     public String adminDashboard(@PathVariable String token) {
-        Map<String, String> validationResult = service.validateToken(token, "admin");
+        ResponseEntity<Map<String, String>> validationResult = service.validateToken(token, "admin");
         
-        if (validationResult.isEmpty()) {
+        if (validationResult.getStatusCode().is2xxSuccessful()) {
             return "admin/adminDashboard";
         } else {
             return "redirect:http://localhost:8080";
@@ -25,9 +29,9 @@ public class DashboardController {
 
     @GetMapping("/doctorDashboard/{token}")
     public String doctorDashboard(@PathVariable String token) {
-        Map<String, String> validationResult = service.validateToken(token, "doctor");
+        ResponseEntity<Map<String, String>> validationResult = service.validateToken(token, "doctor");
         
-        if (validationResult.isEmpty()) {
+        if (validationResult.getStatusCode().is2xxSuccessful()) {
             return "doctor/doctorDashboard";
         } else {
             return "redirect:http://localhost:8080";
