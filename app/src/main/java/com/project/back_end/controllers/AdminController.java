0fd8288778cf9fa8305f8cb6d107c1efaa483ebe
Rashid Amanzholov <rashid.amanzholov@gmail.com>
2025-07-15 17:20:42 +0200

    created Admin Controller

--- app/src/main/java/com/project/back_end/controllers/AdminController.java
+++ app/src/main/java/com/project/back_end/controllers/AdminController.java
@@ -1,7 +1,32 @@
-
 package com.project.back_end.controllers;
 
+import com.project.back_end.models.Admin;
+import com.project.back_end.services.AppService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("${api.path}" + "admin")
 public class AdminController {
+
+    private final AppService appService;
+
+    public AdminController(AppService appService) {
+        this.appService = appService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> adminLogin(@RequestBody Admin admin) {
+        return appService.validateAdmin(admin);
+    }
+}
+
+
+// package com.project.back_end.controllers;
+
+// public class AdminController {
 
 // 1. Set Up the Controller Class:
 //    - Annotate the class with `@RestController` to indicate that it's a REST controller, used to handle web requests and return JSON responses.
@@ -23,5 +48,5 @@ public class AdminController {
 
 
 
-}
+// }
 
